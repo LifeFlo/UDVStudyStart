5a8e6b316b71d2ca8d142cab0a57f8db0da34d5b
Блаблаблаблааа <tokareva_todal@mail.ru>
2023-11-09 02:46:32 +0500

    Add game-map

--- frontned/src/Pages/Game/Game.tsx
+++ frontned/src/Pages/Game/Game.tsx
@@ -1,18 +1,37 @@
 import ModalWithCompanion from "../../component/ModalWithCompanion";
+import styles from "../../component/modal.module.css";
+import React from "react";
 import {useState} from "react";
+import {text} from "stream/consumers";
 export default function Game() {
-    const [isOpen, setIsOpen] = useState(false)
+    // const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div>
+        <div className={styles.background}>
+            {/*<div>*/}
+            {/*    <h1>Game</h1>*/}
+            {/*    <input type={"button"} onClick={() => setIsOpen(!isOpen)} style={{height: "50px", width: "30px"}}/>*/}
+            {/*</div>*/}
+            {/*{isOpen && <ModalWithCompanion  Header={"Приветсвтенная речь"}*/}
+            {/*                                  paragraph={"Равным образом тыры пыры тыры пыры"}/>*/}
+            {/*}*/}
+            <h1 className={styles.text}>GAME</h1>
             <div>
-                <h1>Game</h1>
-                <input type={"button"} onClick={() => setIsOpen(!isOpen)} style={{height: "50px", width: "30px"}}/>
+                <button className={styles.btnImg4}></button>
+                <button className={styles.btnImg6}></button>
             </div>
-            {isOpen && <ModalWithCompanion  Header={"Приветсвтенная речь"}
-                                              paragraph={"Равным образом тыры пыры тыры пыры"}/>
-            }
-        </div>
+            <div>
+                <button className={styles.btnImg1}></button>
+                <button className={styles.btnImg3}></button>
+            </div>
+            <div>
+                <button className={styles.btnImg2}></button>
+                <button className={styles.btnImg5}></button>
+            </div>
 
+
+
+
+        </div>
     );
 }
