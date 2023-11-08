22817a31f27f11286fe2d07843b113679baa2a6c
Блаблаблаблааа <tokareva_todal@mail.ru>
2023-11-09 03:16:00 +0500

    Add game-map

--- frontned/src/Pages/Game/Game.tsx
+++ frontned/src/Pages/Game/Game.tsx
@@ -4,34 +4,28 @@ import React from "react";
 import {useState} from "react";
 import {text} from "stream/consumers";
 export default function Game() {
-    // const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className={styles.background}>
-            {/*<div>*/}
-            {/*    <h1>Game</h1>*/}
-            {/*    <input type={"button"} onClick={() => setIsOpen(!isOpen)} style={{height: "50px", width: "30px"}}/>*/}
-            {/*</div>*/}
-            {/*{isOpen && <ModalWithCompanion  Header={"Приветсвтенная речь"}*/}
-            {/*                                  paragraph={"Равным образом тыры пыры тыры пыры"}/>*/}
-            {/*}*/}
+
             <h1 className={styles.text}>GAME</h1>
             <div>
-                <button className={styles.btnImg4}></button>
-                <button className={styles.btnImg6}></button>
+                <button className={styles.btnImg4} onClick={() => setIsOpen(!isOpen)}></button>
+                <button className={styles.btnImg6} onClick={() => setIsOpen(!isOpen)}></button>
             </div>
             <div>
-                <button className={styles.btnImg1}></button>
-                <button className={styles.btnImg3}></button>
+                <button className={styles.btnImg1} onClick={() => setIsOpen(!isOpen)}></button>
+                <button className={styles.btnImg3} onClick={() => setIsOpen(!isOpen)}></button>
             </div>
             <div>
-                <button className={styles.btnImg2}></button>
-                <button className={styles.btnImg5}></button>
+                <button className={styles.btnImg2} onClick={() => setIsOpen(!isOpen)}></button>
+                <button className={styles.btnImg5} onClick={() => setIsOpen(!isOpen)}></button>
             </div>
 
-
-
-
+            {isOpen && <ModalWithCompanion  Header={"Приветсвтенная речь"}
+                                            paragraph={"Равным образом тыры пыры тыры пыры"}/>
+            }
         </div>
     );
 }
