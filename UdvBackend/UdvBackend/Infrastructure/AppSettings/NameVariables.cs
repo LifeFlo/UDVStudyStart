19026a4ac45e56b4a707d3da392725019ed226ac
Блаблаблаблааа <tokareva_todal@mail.ru>
2023-12-19 12:57:16 +0500

    It`s work

--- UdvBackend/UdvBackend/Infrastructure/AppSettings/NameVariables.cs
+++ UdvBackend/UdvBackend/Infrastructure/AppSettings/NameVariables.cs
@@ -2,5 +2,5 @@ namespace UdvBackend.Infrastructure;
 
 public class NameVariables
 {
-    public static string HostDataBase = "PostgresHost";
+    public static string HostDataBase = "ep-bitter-snowflake-59747597-pooler.us-east-1.postgres.vercel-storage.com";
 }
--- frontned/src/component/ProfileEmp/EmplCard/EmplCard.tsx
+++ frontned/src/component/ProfileEmp/EmplCard/EmplCard.tsx
@@ -3,11 +3,11 @@ import styles from "./emplCard.module.css"
 import axios from "axios";
 
 
-
 export function EmplCard(){
     const [name, setName] = useState()
     const [surname, setSurname] = useState()
-    const [email, setEmail] = useState()
+    const [midl, setMidl] = useState()
+    const [mail, setMail] = useState()
 
     const token = localStorage.getItem('token')
     axios.get(
@@ -24,11 +24,17 @@ export function EmplCard(){
         'http://37.139.43.80:80/api/account',
         {headers: {Authorization: `Bearer ${token}`}},
     )
-        .then(r => setEmail(r.data.value.email))
+        .then(r => setMidl(r.data.value.middleName))
+    axios.get(
+        'http://37.139.43.80:80/api/account',
+        {headers: {Authorization: `Bearer ${token}`}},
+    )
+        .then(r => setMail(r.data.value.email))
 
     let nam = name
     let ser = surname
-    let mail = email
+    let mid = midl
+    let mal = mail
 
 
     const onClickExit = () => {
@@ -38,15 +44,15 @@ export function EmplCard(){
 
     return(
         <div className={styles.card}>
+            <div className={styles.ava}></div>
             <div className={styles.fio}>
-                {nam}
-                {ser}
+                {nam} <span>{ser}</span> <span>{mid}</span>
             </div>
             <div className={styles.contactTitle}>
                 Почта
             </div>
             <div className={styles.contact}>
-                {mail}
+                {mal}
             </div>
             <button
                 className={styles.btnExit}
